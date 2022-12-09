dfb39df6c30f3fcc991aacfc0fe314faeaed68af
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2022-12-08 20:35:06 -0800

    run\

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-
-
 int main(void)
 {
-("\"Programming is like building a multilingual puzzle");
-return (0);
+  ("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
