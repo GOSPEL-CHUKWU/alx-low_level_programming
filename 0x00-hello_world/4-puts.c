bc8e5d1a743176ba408fea814739f530aefc264a
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2022-12-09 09:25:49 -0800

    final change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
-
  * main - Entry point
-
  *
-
+ * Description: 'runing puts'
+ *
  * Return: Always 0 (Success)
-
-*/
+ */
 
 int main(void)
 {
-  ("\"Programming is like building a multilingual puzzle");
-  return (0);
+("\"Programming is like building a multilingual puzzle");
+return (0);
 }
