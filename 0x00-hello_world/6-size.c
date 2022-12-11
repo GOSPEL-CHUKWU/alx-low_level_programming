3bd2748b1a63935a388075a07269959fd1b72bcc
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2022-12-10 16:10:21 -0800

    run

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ long int b;
 long long int c;
 char d;
 float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %c byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %lu bytes(s)", (unsigned long)sizeof(c));
-printf("Size of a float: %lu bytes(s)", (unsigned long)sizeof(f));
+printf("Size of a long long int: %lld bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %f bytes(s)", (unsigned long)sizeof(f));
 return (0);
 }
