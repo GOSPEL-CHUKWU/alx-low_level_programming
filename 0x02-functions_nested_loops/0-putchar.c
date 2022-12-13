8e8659d3d01ca6f2fdc33709da3db6252afb5bf6
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2022-12-13 14:10:22 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A function that prints putchar
+ *
+ * Returns: Always 0
+ */
+
+int main(void)
+{
+printf("_putchar\n")
+
+return 0
+}
