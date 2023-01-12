4f3fb70632d672a341fe7a838a1748a9479df3da
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-01-12 05:44:08 -0800

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,29 +30,22 @@ if (mem == NULL)
 return (NULL);
 
 return (mem);
-
 }
-
 if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
-
 ptr_copy = ptr;
 mem = malloc(sizeof(*ptr_copy) * new_size);
-
 if (mem == NULL)
 {
 fee(ptr);
 return (NULL);
 }
-
 filler = mem;
-
 for (index = 0; index < old_size && index < new_size; index++)
 fller[index] = *ptr_copy++;
-
 free(ptr);
 return (mem);
 }
