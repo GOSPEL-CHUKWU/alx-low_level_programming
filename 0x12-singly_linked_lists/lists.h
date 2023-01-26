d3f3f414049a421d99b6635359eea43cc4361267
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-01-26 15:05:26 -0800

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef _LINKED_LIST
 #define _LINKED_LIST
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
