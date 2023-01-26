4e260541d0dab2cece8d0af5e91872a3e8c1b291
GOSPEL-CHUKWU <gospelchukwu.me@gmail.com>
2023-01-26 14:03:14 -0800

    fixed issue in task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - Show the number of elements of a list
- * @h: A linked list
- *
- * Return: The number of elements of a list
+ * list_len - a function that returns the number
+ *  of elements in a linked list_t list
+ * @h: input
+ * Return: 0
  */
 
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
-while (h)
-h = h->next;
+int count = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
 count++;
+h = h->next;
 }
 return (count);
 }
